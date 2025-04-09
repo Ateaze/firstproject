c16b1be4444f8344e71ecca80d355e0f56f63f3d
Ateaze <jaegwang7827@gmail.com>
2025-04-09 23:29:09 +0900

    firstproject

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.controller;
+
+// 컨트롤러 선언과 동시에 자동으로 임포트
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        // model 객체가 "Velue"값을 "username"에 연결해 웹브라우저로 보냄
+        model.addAttribute("username", "ArtR");
+        return "greetings"; //greetings.mustache 파일 반환
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "ArtR");
+        return "Goodbye";
+    }
+}
