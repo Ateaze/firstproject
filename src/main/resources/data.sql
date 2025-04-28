75a26674c26ed7fbef6d472b70f4b9986099db7f
Ateaze <jaegwang7827@gmail.com>
2025-04-28 22:41:07 +0900

    4/28

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,6 @@ INSERT INTO article(title, content) VALUES ('다다다','3333');
 INSERT INTO member(email, password) VALUES ('admin@naver.com', '456789');
 INSERT INTO member(email, password) VALUES ('admin@gmail.com', '999999');
 INSERT INTO member(email, password) VALUES ('admin@kakao.com', '123456');
+INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES('카페 모카', '5500');
